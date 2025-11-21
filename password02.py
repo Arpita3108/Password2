9d5e3ad16643688eb97b962a8f6aff741cc3cece
ArpitaMB <arpitambiradar3108@gmail.com>
2025-11-21 11:29:37 +0530

    Update password02.py

--- password02.py
+++ password02.py
@@ -5,18 +5,15 @@ if len(sys.argv[0]) ==2:
     password =int(map(sys.argv[0]))
     print("Using command line inputs")
 else:
-    print("No input given using default values")
-
-#Default values
     password = 7677
     confirm_password = 5767
 
 #Confirming the password
-    if password == confirm_password:
+if password == confirm_password:
         print("Match-Confirm password")
-    elif password != confirm_password:
+ elif password != confirm_password:
         print("Mismatch,try again")
-    else:
+else:
         print("Reset password")
 
 #Display result
